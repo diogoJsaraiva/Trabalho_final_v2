9ae576f9f6a357643477a13f3f14eb467b4eb66b
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-07-13 18:45:03 +0100

    Criação da tabela vacina

---
+++ app/src/main/java/com/example/trabalho_final_v2/TabelaVacina.kt
@@ -0,0 +1,46 @@
+package com.example.trabalho_final_v2
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaVacinas (db: SQLiteDatabase) : BaseColumns {
+    private val db : SQLiteDatabase = db
+    fun cria() = db?.execSQL(
+        "CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME_VACINA TEXT NOT NULL, $CAMPO_LOTE NUMBER NOT NULL)"
+    )
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        groupBy: String?,
+        having: String?,
+        orderBy: String?
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_TABELA = "vacina"
+        const val CAMPO_NOME_VACINA = "nome_vacina"
+        const val CAMPO_LOTE = "lote"
+
+        val TODOS_CAMPOS =arrayOf(BaseColumns._ID, CAMPO_NOME_VACINA)
+
+    }
+
+}
