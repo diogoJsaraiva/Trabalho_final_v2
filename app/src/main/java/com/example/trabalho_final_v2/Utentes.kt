65646cd7229c628687e99b3669b82287078dc928
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-07-13 19:01:03 +0100

    Criação do objeto Utentes.kt

---
+++ app/src/main/java/com/example/trabalho_final_v2/Utentes.kt
@@ -0,0 +1,41 @@
+package com.example.trabalho_final_v2
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+import java.util.*
+
+
+data class Utentes(var id: Long = -1, var nome: String, var telefone: String, var contribuinte: Int, var dataNascimento: Date, var dose: Int) {
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues().apply {
+            put(TabelaUtentes.CAMPO_NOME, nome)
+            put(TabelaUtentes.CAMPO_TELEFONE, telefone)
+            put(TabelaUtentes.CAMPO_CONTRIBUINTE, contribuinte)
+            put(TabelaUtentes.CAMPO_DATA_NASCIMENTO, dataNascimento.time)
+            put(TabelaUtentes.CAMPO_DOSE, dose)
+        }
+
+        return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Utentes {
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colNome = cursor.getColumnIndex(TabelaUtentes.CAMPO_NOME)
+            val colTelefone = cursor.getColumnIndex(TabelaUtentes.CAMPO_TELEFONE)
+            val colContribuinte = cursor.getColumnIndex(TabelaUtentes.CAMPO_CONTRIBUINTE)
+            val colDataNascimento = cursor.getColumnIndex(TabelaUtentes.CAMPO_DATA_NASCIMENTO)
+            val colDose = cursor.getColumnIndex(TabelaUtentes.CAMPO_DOSE)
+
+            val id = cursor.getLong(colId)
+            val nome = cursor.getString(colNome)
+            val telefone = cursor.getString(colTelefone)
+            val contribuinte = cursor.getInt(colContribuinte)
+            val dataNascimento = cursor.getLong(colDataNascimento)
+            val dose = cursor.getInt(colDose)
+
+            return Utentes(id, nome, telefone, contribuinte, Date(dataNascimento), dose)
+        }
+    }
+}
