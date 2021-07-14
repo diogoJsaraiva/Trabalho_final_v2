80b6ea75299ac4d76a5223c7db2f711b51a8dbc6
Diogo Saraiva <diogo.saraiva12@hotmail.com>
2021-07-14 23:48:35 +0100

    Criação dos DAdos app

---
+++ app/src/main/java/com/example/trabalho_final_v2/DadosApp.kt
@@ -0,0 +1,11 @@
+package com.example.trabalho_final_v2
+
+import androidx.fragment.app.Fragment
+
+class DadosApp {
+    companion object {
+        lateinit var activity: MainActivity
+        lateinit var fragment: Fragment
+        var vacinasSelecionado : Vacinas? = null
+    }
+}
